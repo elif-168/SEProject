c716c8c7ab1e482910d2d0a7cfcefb1ec36ba7e3
Elif Gülerarslan <109799089+elif-168@users.noreply.github.com>
2025-05-02 19:41:52 +0300

    Initial commit

---
+++ SEProject/src/LoginController.java
@@ -0,0 +1,17 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class LoginController {
+
+    @FXML
+    private void login(ActionEvent event) {
+        // TODO: Handle login logic here
+        System.out.println("Login button clicked");
+    }
+
+    @FXML
+    private void continueWithoutAccount(ActionEvent event) {
+        // TODO: Handle continue without account logic here
+        System.out.println("Continue without account clicked");
+    }
+}
